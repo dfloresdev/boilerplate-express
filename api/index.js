7ec339d0c9ce4e320f5620540a8a0d34ad68d036
dfloresdev <hello@dflores.dev>
2020-04-11 02:05:44 -0500

    [DOCUMENTATION] add documentation about endpoints

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const swaggerUI = require("swagger-ui-express");
+
 const config = require("../config");
 const employee = require("./components/employee/network");
 
 const app = express();
 app.use(bodyParser.json());
+const swaggerDoc = require("./swagger.json");
 
 // ROUTER
 app.use("/api/employee", employee);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
   console.log(
